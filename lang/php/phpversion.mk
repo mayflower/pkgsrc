0bacf51cd33444e43dc5a560546d652558b6758b
taca <taca@pkgsrc.org>
2018-08-19 13:55:24 +0000

    lang/php71: Update to 7.1.21
    PHP                                                                        NEWS
    |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
    16 Aug 2018, PHP 7.1.21
    
    - Calendar:
      . Fixed bug #52974 (jewish.c: compile error under Windows with GBK charset).
        (cmb)
    
    - Filter:
      . Fixed bug #76366 (References in sub-array for filtering breaks the filter).
        (ZiHang Gao)
    
    - PDO_Firebird:
      . Fixed bug #76488 (Memory leak when fetching a BLOB field). (Simonov Denis)
    
    - PDO_PgSQL:
      . Fixed bug #75402 (Possible Memory Leak using PDO::CURSOR_SCROLL option).
        (Anatol)
    
    - SQLite3:
      . Fixed #76665 (SQLite3Stmt::bindValue() with SQLITE3_FLOAT doesn't juggle).
        (cmb)
    
    - Standard:
      . Fixed bug #68553 (array_column: null values in $index_key become incrementing
        keys in result). (Laruence)
      . Fixed bug #73817 (Incorrect entries in get_html_translation_table). (cmb)
      . Fixed bug #76643 (Segmentation fault when using `output_add_rewrite_var`).
        (cmb)
    
    - Zip:
      . Fixed bug #76524 (ZipArchive memory leak (OVERWRITE flag and empty archive)).
        (Timur Ibragimov)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.37
 PHP70_VERSION=	7.0.31
-PHP71_VERSION=	7.1.20
+PHP71_VERSION=	7.1.21
 PHP72_VERSION=	7.2.8
 
 # Define initial release of major version.
