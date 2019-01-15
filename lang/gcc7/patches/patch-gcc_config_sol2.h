698114be2f7caf9b384710c84cbcb092a5bb1bc1
Jonathan Perkin <jperkin@joyent.com>
2019-01-15 20:25:17 +0000

    gcc{49,7}: Test tools path fixes from miscfix

--- lang/gcc7/patches/patch-gcc_config_sol2.h
+++ lang/gcc7/patches/patch-gcc_config_sol2.h
@@ -11,7 +11,7 @@ Pull in ${PREFIX}/lib by default and handle for multiarch.
 -   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp%R/lib:%R/usr/lib} \
 -	   %{!p:%{!pg:-Y P,%R/lib:%R/usr/lib}}}"
 +   -R@PREFIX@/lib \
-+   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp:%R/lib:%R/usr/lib:%R@PREFIX@/lib} \
++   %{!YP,*:%{p|pg:-Y P,%R/lib:%R/usr/lib:%R@PREFIX@/lib} \
 +	   %{!p:%{!pg:-Y P,%R/lib:%R/usr/lib:%R@PREFIX@/lib}}}"
  
  #undef LINK_ARCH32_SPEC
@@ -23,7 +23,7 @@ Pull in ${PREFIX}/lib by default and handle for multiarch.
 -   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR "}	\
 -	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR "}}}"
 +   -R@PREFIX@/lib/" @MARCH64_SUBDIR@ " \
-+   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib/" @MARCH64_SUBDIR@ "} \
++   %{!YP,*:%{p|pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib/" @MARCH64_SUBDIR@ "} \
 +	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib/" @MARCH64_SUBDIR@ "}}}"
  
  #undef LINK_ARCH64_SPEC
