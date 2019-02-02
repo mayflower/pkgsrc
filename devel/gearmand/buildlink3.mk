1d5b628c17662d701f178c2a87d4f676d23f2921
Michael Moll <michael.moll@mayflower.de>
2019-02-02 21:19:36 +0100

    Revert "devel/gearmand: Update to 1.1.18."
    This reverts commit b6702b85abf773c1bd1d7cf98fed100aaf94fefd.

--- devel/gearmand/buildlink3.mk
+++ devel/gearmand/buildlink3.mk
@@ -6,14 +6,13 @@ BUILDLINK_TREE+=	gearmand
 GEARMAND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gearmand+=	gearmand>=1.0.6nb17
-BUILDLINK_ABI_DEPENDS.gearmand+=	gearmand>=1.1.18nb2
+BUILDLINK_ABI_DEPENDS.gearmand+=	gearmand>=1.0.6nb29
 BUILDLINK_PKGSRCDIR.gearmand?=		../../devel/gearmand
 
 .include "../../devel/boost-headers/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
 .include "../../devel/libevent/buildlink3.mk"
 .include "../../devel/libexecinfo/buildlink3.mk"
-.include "../../devel/libuuid/buildlink3.mk"
 .include "../../security/libssh2/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .endif	# GEARMAND_BUILDLINK3_MK
